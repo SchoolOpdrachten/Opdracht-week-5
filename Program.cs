0d03004c433e05bc768304e98ef3d4730f684bb9
Petzr <peterjansen812@gmail.com>
2022-10-16 14:30:26 +0200

    copypast is mijn vriend

--- Program.cs
+++ Program.cs
@@ -1,44 +1,106 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
 
-namespace Pretpark
+namespace Pretpark;
+
+internal class Program
 {
-    class Program
+    static string? userAgent;
+    
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var server = new TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), 5000);
+        server.Start();
+        
+        var teller = 0;
+
+        while (true)
         {
-            TcpListener server = new TcpListener(new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }), 5000);
-            server.Start();
-            while (true)
+            using var connectie = server.AcceptSocket();
+            using Stream request = new NetworkStream(connectie);
+            using var requestLezer = new StreamReader(request);
+
+            var regel1 = requestLezer.ReadLine()?.Split(" ");
+            if (regel1 == null) continue;
+
+            (var methode, var url, var httpversie) = (regel1[0], regel1[1], regel1[2]);
+            var regel = requestLezer.ReadLine();
+            var contentLength = 0;
+            while (!string.IsNullOrEmpty(regel) && !requestLezer.EndOfStream)
             {
-                using Socket connectie = server.AcceptSocket();
-                using Stream request = new NetworkStream(connectie);
-                using StreamReader requestLezer = new StreamReader(request);
-
-                string[]? regel1 = requestLezer.ReadLine()?.Split(" ");
-                if (regel1 == null) return;
-
-                (string methode, string url, string httpversie) = (regel1[0], regel1[1], regel1[2]);
-                string? regel = requestLezer.ReadLine();
-                int contentLength = 0;
-                while (!string.IsNullOrEmpty(regel) && !requestLezer.EndOfStream)
+                var regelstukje = regel.Split(":");
+                var (header, waarde) = (regelstukje[0], regelstukje[1]);
+                if (header.ToLower() == "content-length")
+                    contentLength = int.Parse(waarde);
+                regel = requestLezer.ReadLine(); 
+                
+                if (header.ToLower() == "content-length")
+                    contentLength = int.Parse(waarde);
+                  
+                regel = requestLezer.ReadLine();
+                if (header == "User-Agent")
                 {
-                    string[] stukjes = regel.Split(":");
-                    (string header, string waarde) = (stukjes[0], stukjes[1]);
-                    if (header.ToLower() == "content-length")
-                        contentLength = int.Parse(waarde);
-                    regel = requestLezer.ReadLine();
+                    userAgent = waarde;
                 }
-                if (contentLength > 0)
-                {
-                    char[] bytes = new char[(int)contentLength];
-                    requestLezer.Read(bytes, 0, (int)contentLength);
-                }
-
-                string content = System.IO.File.ReadAllText("html.html");
-
-                connectie.Send(System.Text.Encoding.ASCII.GetBytes($"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
             }
 
+            if (contentLength > 0)
+            {
+                var bytes = new char[contentLength];
+                requestLezer.Read(bytes, 0, contentLength);
+            }
+
+            if (url == "/contact")
+            {
+                var content = File.ReadAllText("contact.html");
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
+            }
+            else if (url == "/teller")
+            {
+                teller++;
+                var content = "Teller: " + teller;
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
+            }
+            else if (url.Contains("/mijnteller"))
+            {
+                var myUri = new Uri("http://localhost:5000" + url);
+                var t = 0;
+                if (url.Contains("?t="))
+                {
+                    t = int.Parse(HttpUtility.ParseQueryString(myUri.Query).Get("t"));
+                }
+
+                var content = "De teller staat op " + t + ", klik <a href='mijnteller?t=" + (t+1) + "'>hier</a> om te verhogen";
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
+            }
+            else if (url.Contains("/add"))
+            {
+                var myUri = new Uri("http://localhost:5000" + url);
+                var a = int.Parse(HttpUtility.ParseQueryString(myUri.Query).Get("a"));
+                var b = int.Parse(HttpUtility.ParseQueryString(myUri.Query).Get("b"));
+                var som = a + b;
+                var content = "Som: " + som;
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
+            }
+            else if (url == "/" || url == "")
+            {
+                Console.WriteLine(httpversie);
+                var content = File.ReadAllText("index.html") + userAgent;
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content.Length}\r\n\r\n{content}"));
+            }
+            else
+            {
+                var content = "<h1>404 Not Found</h1><p>Pagina niet gevonden</p>";
+                connectie.Send(Encoding.ASCII.GetBytes(
+                    $"HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 49\r\n\r\n{content}"));
+            }
         }
     }
 }
